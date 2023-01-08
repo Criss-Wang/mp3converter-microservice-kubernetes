39020db4b3a23a3eab9df4c248e9389a195e5954
Zhenlin Wang <djj87018507@gmail.com>
2023-01-08 00:57:12 -0500

    create rabbitmq service

--- src/gateway/storage/util.py
+++ src/gateway/storage/util.py
@@ -1,21 +1,31 @@
-import os, requests
+import pika
+import json
 
 
-def token(request):
-    if not "Authorization" in request.headers:
-        return None, ("missing credentials", 401)
+def upload(f, fs, channel, access):
+    try:
+        # saving to mongodb and get a file id
+        fid = fs.put(f)
+    except Exception as err:
+        print(err)
+        return "internal server error", 500
 
-    token = request.headers["Authorization"]
+    message = {
+        "video_fid": str(fid),
+        "mp3_fid": None,
+        "username": access["username"],
+    }
 
-    if not token:
-        return None, ("missing credentials", 401)
-
-    response = requests.post(
-        f"http://{os.environ.get('AUTH_SVC_ADDRESS')}/validate",
-        headers={"Authorization": token},
-    )
-
-    if response.status_code == 200:
-        return response.text, None
-    else:
-        return None, (response.text, response.status_code)
+    try:
+        channel.basic_publish(
+            exchange="", # default exchange
+            routing_key="video",
+            body=json.dumps(message),
+            properties=pika.BasicProperties(
+                delivery_mode=pika.spec.PERSISTENT_DELIVERY_MODE
+            ),
+        )
+    except Exception as err:
+        print(err)
+        fs.delete(fid)
+        return "internal server error", 500
