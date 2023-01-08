b1866e8b17a5cf20da72dcb0dba134b84bb3e4e0
Zhenlin Wang <djj87018507@gmail.com>
2023-01-08 00:17:31 -0500

    Create service code for gateway

--- src/gateway/server.py
+++ src/gateway/server.py
@@ -7,3 +7,22 @@ from storage import util
 from bson.objectid import ObjectId
 
 server = Flask(__name__)
+
+mongo_video = PyMongo(server, uri="mongodb://host.minikube.internal:27017/videos")
+
+mongo_mp3 = PyMongo(server, uri="mongodb://host.minikube.internal:27017/mp3s")
+
+fs_videos = gridfs.GridFS(mongo_video.db)
+fs_mp3s = gridfs.GridFS(mongo_mp3.db)
+
+connection = pika.BlockingConnection(pika.ConnectionParameters("rabbitmq"))
+channel = connection.channel()
+
+@server.route("/login", methods=["POST"])
+def login():
+    token, err = access.login(request)
+
+    if not err:
+        return token
+    else:
+        return err
